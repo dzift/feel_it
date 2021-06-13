62f7eb3a318a98f770ac2a51540ea218e69789e0
kub1st <dvbelockii@gmail.com>
2021-06-14 01:00:49 +1000

    2

--- js/scripts.js
+++ js/scripts.js
@@ -9,19 +9,19 @@
 
 window.addEventListener('DOMContentLoaded', event => {
 
-    // // Navbar shrink function
-    // var navbarShrink = function () {
-    //     const navbarCollapsible = document.body.querySelector('#mainNav');
-    //     if (!navbarCollapsible) {
-    //         return;
-    //     }
-    //     if (window.scrollY === 0) {
-    //         navbarCollapsible.classList.remove('navbar-shrink')
-    //     } else {
-    //         navbarCollapsible.classList.add('navbar-shrink')
-    //     }
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
 
-    // };
+    };
 
     // Shrink the navbar 
     navbarShrink();
