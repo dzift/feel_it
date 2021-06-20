e23f801e8ee6f8d86289885629e420a3521acd3a
kub1st <dvbelockii@gmail.com>
2021-06-20 22:54:07 +1000

    h

--- js/scripts.js
+++ js/scripts.js
@@ -26,8 +26,78 @@ window.addEventListener('DOMContentLoaded', event => {
     // // Shrink the navbar 
     // navbarShrink();
 
-    // Shrink the navbar when page is scrolled
-    document.addEventListener('scroll', navbarShrink);
+    var $card1 = document.getElementById("feelCard_1");
+    var $card2 = document.getElementById("feelCard_2");
+    var $card3 = document.getElementById("feelCard_3");
+    var a = randomInteger(1, 32)
+    var b = randomInteger(1, 32)
+    var c = randomInteger(1, 32)
+
+    var html1 = `<!-- Project details-->
+    <h3 class="text-uppercase">Желаемое состояние</h3>
+    <p class="item-intro text-muted">Какого состояния мне бы хотелось достичь?</p>
+    <img class="img-fluid d-block mx-auto" src="assets/img/card/card/1 (${a}).png" alt="..." />
+    <p><strong>Вспомогательные вопросы для работы с картой:</strong></p>
+    <p> Что я чувствую, глядя на карту? Что на карте привлекло мое внимание?
+        Почему? Какие эмоции у меня возникают?
+        В каких ситуациях, которые происходили ранее были похожие эмоции?
+        Как чувствует себя персонаж на картинке? Что персонаж (или персонажи) будет делать
+        дальше?
+        Как я к этому отношусь? Вижу ли я на картинке себя?
+        В какой роли я здесь выступаю? Что я не хочу видеть? Почему?
+        Как то, что изображено на карте, говорит мне о моем запросе? </p>
+    <button class="btn btn-primary btn-xl text-uppercase" data-bs-dismiss="modal"
+        type="button">
+        <i class="fas fa-times me-1"></i>
+        ЗАКРЫТЬ
+    </button>`
+
+    var html2 = `<!-- Project details-->
+    <h3 class="text-uppercase">Желаемое состояние</h3>
+    <p class="item-intro text-muted">Какого состояния мне бы хотелось достичь?</p>
+    <img class="img-fluid d-block mx-auto" src="assets/img/card/card/1 (${b}).png" alt="..." />
+    <p><strong>Вспомогательные вопросы для работы с картой:</strong></p>
+    <p> Что я чувствую, глядя на карту? Что на карте привлекло мое внимание?
+        Почему? Какие эмоции у меня возникают?
+        В каких ситуациях, которые происходили ранее были похожие эмоции?
+        Как чувствует себя персонаж на картинке? Что персонаж (или персонажи) будет делать
+        дальше?
+        Как я к этому отношусь? Вижу ли я на картинке себя?
+        В какой роли я здесь выступаю? Что я не хочу видеть? Почему?
+        Как то, что изображено на карте, говорит мне о моем запросе? </p>
+    <button class="btn btn-primary btn-xl text-uppercase" data-bs-dismiss="modal"
+        type="button">
+        <i class="fas fa-times me-1"></i>
+        ЗАКРЫТЬ
+    </button>`
+
+    var html3 = `<!-- Project details-->
+    <h3 class="text-uppercase">Желаемое состояние</h3>
+    <p class="item-intro text-muted">Какого состояния мне бы хотелось достичь?</p>
+    <img class="img-fluid d-block mx-auto" src="assets/img/card/card/1 (${c}).png" alt="..." />
+    <p><strong>Вспомогательные вопросы для работы с картой:</strong></p>
+    <p> Что я чувствую, глядя на карту? Что на карте привлекло мое внимание?
+        Почему? Какие эмоции у меня возникают?
+        В каких ситуациях, которые происходили ранее были похожие эмоции?
+        Как чувствует себя персонаж на картинке? Что персонаж (или персонажи) будет делать
+        дальше?
+        Как я к этому отношусь? Вижу ли я на картинке себя?
+        В какой роли я здесь выступаю? Что я не хочу видеть? Почему?
+        Как то, что изображено на карте, говорит мне о моем запросе? </p>
+    <button class="btn btn-primary btn-xl text-uppercase" data-bs-dismiss="modal"
+        type="button">
+        <i class="fas fa-times me-1"></i>
+        ЗАКРЫТЬ
+    </button>`
+    $card1.insertAdjacentHTML("beforeBegin", html1);
+    $card2.insertAdjacentHTML("beforeBegin", html2);
+    $card3.insertAdjacentHTML("beforeBegin", html3);
+    
+
+
+
+    // // Shrink the navbar when page is scrolled
+    // document.addEventListener('scroll', navbarShrink);
 
     // Activate Bootstrap scrollspy on the main nav element
     const mainNav = document.body.querySelector('#mainNav');
@@ -52,3 +122,12 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+
+
+    function randomInteger(min, max) {
+        // случайное число от min до (max+1)
+        let rand = min + Math.random() * (max + 1 - min);
+        return Math.floor(rand);
+      }
+      
